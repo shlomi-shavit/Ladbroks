bce035c0a8afd2d76cfdbbead93e309b76ce7d03
shlomi-shavit <shlomishavit8@gmail.com>
2017-09-24 01:58:08 +0300

    Add files via upload

---
+++ builds/dev/js/script.js
@@ -0,0 +1,15 @@
+
+/*---- Toggle active class on mobile button  ----*/
+/*var mobileBtn = document.getElementsByClassName("mobile-btn")[0];
+
+mobileBtn.onclick = function() {
+    if (mobileBtn.className == "mobile-btn active"){
+        mobileBtn.className = 'mobile-btn';
+    } else {
+        mobileBtn.className = mobileBtn.className + ' active';
+    }
+}*/
+/*-----------------------------------------------*/
+"use strict";
+
+//# sourceMappingURL=script.js.map
---
+++ process/js/script.js
@@ -0,0 +1,12 @@
+
+/*---- Toggle active class on mobile button  ----*/
+/*var mobileBtn = document.getElementsByClassName("mobile-btn")[0];
+
+mobileBtn.onclick = function() {
+    if (mobileBtn.className == "mobile-btn active"){
+        mobileBtn.className = 'mobile-btn';
+    } else {
+        mobileBtn.className = mobileBtn.className + ' active';
+    }
+}*/
+/*-----------------------------------------------*/
